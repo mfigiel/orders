46705a4c0a93d8d8b3fcedadfaadf4efafceb97e
mfigiel <michal.figiel@smava.de>
2020-03-03 15:43:55 +0100

    add tests

--- src/test/java/com/testing/api/controller/OrderControllerTest.java
+++ src/test/java/com/testing/api/controller/OrderControllerTest.java
@@ -3,29 +3,27 @@ package com.testing.api.controller;
 import com.testing.api.resource.OrderApi;
 import com.testing.repository.OrderRepository;
 import com.testing.repository.entity.Order;
-import lombok.var;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -33,6 +31,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class OrderControllerTest {
+
+    private static final String HTTP_LOCALHOST = "http://localhost:";
 
     @Autowired
     private MockMvc mockMvc;
@@ -47,77 +47,77 @@ public class OrderControllerTest {
     private OrderRepository orderRepository;
 
     @Test
-    public void productGet() throws Exception {
+    public void getOrder_NoResults() throws Exception {
         mockMvc
                 .perform(get("/order", 1)
                         .param("id", "1"))
                 .andExpect(status().is4xxClientError());
 
-        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/order/?id=1",
+        assertThat(this.testRestTemplate.getForObject(HTTP_LOCALHOST + port + "/order/?id=1",
                 String.class)).isNotNull();
     }
 
     @Test
-    public void findsTaskById_EmptyData() {
-        try {
-            var order = testRestTemplate.getForObject("http://localhost:" + port + "/order/1", OrderApi.class);
-
-            // assert
-            assertThat(false);
-        } catch (Exception e) {
-            assertThat(true);
-        }
-    }
-
-    @Test
-    public void findsTaskById_dataFill() {
+    public void getOrder() {
         // act
 
-        Order order = getTestOrder();
+        Order order = getTestOrder(1);
 
         orderRepository.save(order);
 
-        var orderFromDb = testRestTemplate.getForObject("http://localhost:" + port + "/order/1", OrderApi.class);
+        OrderApi orderFromDb = testRestTemplate.getForObject(HTTP_LOCALHOST + port + "/order/1", OrderApi.class);
 
-        // assert
-        assertThat(orderFromDb)
-                .extracting(OrderApi::getId, OrderApi::getClientId, OrderApi::getOrderDate, OrderApi::getProducts)
-                .containsExactly(1L, 2, null, order.getProducts());
+        assertEquals(orderFromDb.getId(), 1);
+        assertEquals(orderFromDb.getClientId(), 2);
+        assertEquals(orderFromDb.getOrderDate(), null);
+        assertEquals(orderFromDb.getProducts().size(), 1);
     }
 
     @Test
     public void getOrders() {
         // act
 
-        Order order = getTestOrder();
+        orderRepository.save(getTestOrder(1));
+        orderRepository.save(getTestOrder(2));
 
-        orderRepository.save(order);
-        orderRepository.save(order);
-
-        List<OrderApi> orderFromDb = testRestTemplate.exchange("http://localhost:" + port + "/orders",  HttpMethod.GET, null, new ParameterizedTypeReference<List<OrderApi>>() {
+        ResponseEntity<List<OrderApi>> orderFromDb = testRestTemplate.exchange(HTTP_LOCALHOST + port + "/orders",  HttpMethod.GET, null, new ParameterizedTypeReference<List<OrderApi>>() {
         });
 
         // assert
-        assertThat(orderFromDb.size() == 2);
+        assertEquals(orderFromDb.getBody().size(), 2);
+        OrderApi order = orderFromDb.getBody().get(0);
+        assertEquals(order.getId(), 1);
+        assertEquals(order.getClientId(), 2);
+        assertEquals(order.getOrderDate(), null);
+        assertEquals(order.getProducts().size(), 1);
 
+        OrderApi secondOrder = orderFromDb.getBody().get(1);
+        assertEquals(secondOrder.getId(), 2);
+        assertEquals(secondOrder.getClientId(), 2);
+        assertEquals(secondOrder.getOrderDate(), null);
+        assertEquals(secondOrder.getProducts().size(), 1);
     }
 
     @Test
     public void addOrder() {
         // act
-        OrderApi order = getTestOrderApi();
-        testRestTemplate.postForObject("http://localhost:" + port + "/orders", order, OrderApi.class);
+        OrderApi order = getTestOrderApi(1);
+        testRestTemplate.postForObject(HTTP_LOCALHOST + port + "/orders", order, OrderApi.class);
 
 
-        Optional<Order> orderFromDb = orderRepository.findById(1L);
+        Optional<Order> orderFromDbOptional = orderRepository.findById(1L);
 
+        Order orderFromDb = orderFromDbOptional.get();
         // assert
-        assertThat(orderFromDb.get() != null);
+        assertEquals(orderFromDb.getId(), 1);
+        assertEquals(orderFromDb.getClientId(), 2);
+        assertEquals(orderFromDb.getOrderDate(), null);
+        assertEquals(orderFromDb.getProducts().size(), 1);
     }
 
-    private OrderApi getTestOrderApi() {
+    private OrderApi getTestOrderApi(long id) {
         OrderApi order = new OrderApi();
-        order.setId(1);
+        order.setId(id);
         List<String> productList = new ArrayList<>();
         productList.add("1");
         order.setProducts(productList);
@@ -127,9 +127,9 @@ public class OrderControllerTest {
         return order;
     }
 
-    private Order getTestOrder() {
+    private Order getTestOrder(long id) {
         Order order = new Order();
-        order.setId(1);
+        order.setId(id);
         List<String> productList = new ArrayList<>();
         productList.add("1");
         order.setProducts(productList);
--- src/test/java/com/testing/services/OrderServiceClass.java
+++ src/test/java/com/testing/services/OrderServiceClass.java
@@ -1,6 +1,5 @@
 package com.testing.services;
 
-import com.testing.api.mapping.OrderApiOrderMapper;
 import com.testing.api.resource.OrderApi;
 import com.testing.repository.OrderRepository;
 import com.testing.repository.entity.Order;
@@ -9,25 +8,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 public class OrderServiceClass {
-    @TestConfiguration
-    static class OrderServiceImplTestContextConfiguration {
-
-        @Bean
-        public OrderService orderService() {
-            return new OrderService();
-        }
-    }
 
     @Autowired
     private OrderService orderService;
@@ -43,15 +35,32 @@ public class OrderServiceClass {
 
         Mockito.when(orderRepository.findById((long) 1))
                 .thenReturn(Optional.of(order));
+
+        List<Order> orderList = new ArrayList<>();
+
+        Order orderSecond = new Order();
+        orderSecond.setId(2);
+        orderList.add(orderSecond);
+        Mockito.when(orderRepository.findAll())
+                .thenReturn(orderList);
     }
 
     @Test
-    public void whenValidName_thenEmployeeShouldBeFound() {
+    public void getOneOrder() {
         OrderApi found = orderService.getOrder(1);
 
         assertThat(found.getClientId())
                 .isEqualTo(2);
         assertThat(found.getId())
                 .isEqualTo(1);
+    }
+
+    @Test
+    public void getAllOrders() {
+        List<OrderApi> found = orderService.getOrders();
+
+        assertEquals(found.size(), 2);
+        assertEquals(found.get(0).getId(), 1);
+        assertEquals(found.get(1).getId(), 2);
     }
 }
